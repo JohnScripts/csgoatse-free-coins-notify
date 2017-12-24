43da8254abe99224048211ab4848396870c40fa3
John Scripts <34811618+JohnScripts@users.noreply.github.com>
2017-12-24 13:48:45 +0000

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Csgoatse Free Coins Notify
 // @namespace    https://github.com/JohnScripts
-// @version      1.0
+// @version      1.1
 // @description  Notify Free Coins on CSGOatse
 // @author       John Scripts
 // @match        https://csgoatse.com/*
@@ -15,7 +15,7 @@
 
     var checkExist = setInterval(function() {
         if ($('.free-coins-switcher').text().indexOf('Collect') > -1) {
-            alert("MORE COINS!");
+            alert("FREE COINS!");
             clearInterval(checkExist);
         }
     }, 250);
